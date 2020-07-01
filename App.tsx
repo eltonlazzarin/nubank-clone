b2aa9ab54e2e61c2613a41307be97f66de6f1799
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-06-30 22:59:58 -0300

    main project file

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "./src/pages/Main";
+import { StatusBar } from "react-native";
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
+      <Main />
+    </>
+  );
+};
+
+export default App;
