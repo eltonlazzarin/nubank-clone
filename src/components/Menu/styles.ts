201c7012d8855ff9263649299641026bfc0f453f
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-07-02 15:12:08 -0300

    menu styles

---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,62 @@
+import { StyleSheet, Animated } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled(Animated.ScrollView)`
+  margin: 0 30px;
+`;
+
+export const Code = styled.View`
+  background-color: #fff;
+  padding: 8px;
+  align-self: center;
+`;
+
+export const InfoContainer = styled.View`
+  margin-top: 12px;
+`;
+
+export const AccountInfo = styled.Text`
+  font-size: 13.5px;
+  color: #fff;
+  text-align: center;
+  margin-top: 5px;
+`;
+
+export const Strong = styled.Text`
+  font-weight: bold;
+`;
+
+export const Nav = styled.View`
+  margin-top: 30px;
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  border-top-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 0;
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-bottom-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavText = styled.Text`
+  font-size: 15px;
+  color: #fff;
+  margin-left: 20px;
+`;
+
+export const SignOutButton = styled.TouchableOpacity`
+  border-width: ${StyleSheet.hairlineWidth}px;
+  border-color: rgba(255, 255, 255, 0.8);
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  margin-top: 15px;
+`;
+
+export const SignOutButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 13px;
+`;
