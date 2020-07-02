694b726a90fd06ce023982555333bc85dca099d2
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-07-01 22:23:57 -0300

    chore: implementing animations

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Main from "./src/pages/Main";
-import { StatusBar } from "react-native";
+import React from 'react';
+import Main from './src/pages/Main';
+import { StatusBar } from 'react-native';
 
 const App = () => {
   return (
